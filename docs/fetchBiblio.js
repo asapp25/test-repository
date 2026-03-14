d363b5974279264027f0bae4a34b091a18011231
Adam Sapp <30439118+asapp25@users.noreply.github.com>
2026-03-14 14:15:40 -0400

    Update fetchBiblio.js
    new string method

--- docs/fetchBiblio.js
+++ docs/fetchBiblio.js
@@ -18,21 +18,26 @@ async function fetchAndDisplayBiblioData(){
             const listItem = document.createElement('li');
             const pubDate = new Date(element.meta.parsedDate)
             const pubYear = pubDate.getFullYear();
-            const doiUrl = "http://google.com";//element.data.url;
-            const doiText = "text text";//element.data.DOI;
+            const doiUrl = element.data.url;
+            const doiText = element.data.DOI;
             const a = document.createElement('a');
             a.href = doiUrl;
             a.textContent = doiText;
             a.target = "_blank";
             //a.setAttribute('href',doiUrl);
             //a.innerHTML = doiText;
+            const authors = element.meta.creatorSummary;
+            const title = element.data.title;
+            const pubTitle = element.data.publicationTitle;
 
-            listItem.textContent = `${element.meta.creatorSummary}. `
-                            + pubYear + `. `
-                            + `${element.data.title}. `
-                            + `${element.data.publicationTitle}. `
-                            + `(DOI: `;
-            listItem.appendChild(a);
+            listItem.textContent = authors + ' ' +pubyear + ' ' + title + ' ' + pubTitle + ' (DOI: <a href="' + doiUrl + '" target="_blank">' + doiText + '</a>)';
+
+            //listItem.textContent = `${element.meta.creatorSummary}. `
+            //                + pubYear + `. `
+            //               + `${element.data.title}. `
+            //               + `${element.data.publicationTitle}. `
+            //                + `(DOI: `;
+            //listItem.appendChild(a);
             //listItem.appendChild(")");
             
             pubList.appendChild(listItem);
