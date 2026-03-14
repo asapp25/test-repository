672dd0aa028fc98415e5e87da9513d70069517fe
asapp25 <asapp@uga.edu>
2026-03-14 13:15:52 -0400

    a

--- docs/fetchBiblio.js
+++ docs/fetchBiblio.js
@@ -19,7 +19,7 @@ async function fetchAndDisplayBiblioData(){
             const pubDate = new Date(element.meta.parsedDate)
             const pubYear = pubDate.getFullYear();
             const doiUrl = element.data.url;
-            const doiText = element.data.DOI;
+            const doiText = 'test';//element.data.DOI;
             const a = document.createElement('a');
             a.href = doiUrl;
             a.textContent = doiText;
@@ -32,7 +32,7 @@ async function fetchAndDisplayBiblioData(){
                             + `${element.data.title}. `
                             + `${element.data.publicationTitle}. `
                             + `(DOI: ` + listItem.appendChild(a) + `)`;
-                            
+
             pubList.appendChild(listItem);
             
             //console.log(element.data.title)
